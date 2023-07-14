2cbe59590a3836652a07122155a2fce3a0123ea9
Alexandre Halas <alexandrehalas@gmail.com>
2023-07-14 20:12:13 -0300

    Enhanced object literals

--- script.js
+++ script.js
@@ -4,6 +4,23 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,48 +29,42 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  //before ES6
 
-  order: function (starterIndex, mainIndex) {
+  //openingHours: openingHours,
+
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  //ES6 Enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    time: t,
-    address,
-    mainIndex = 0,
-    starterIndex = 0,
-  }) {
+  orderDelivery({ time: t, address, mainIndex = 0, starterIndex = 0 }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${t}`
     );
   },
 
-  orderPasta: function (ingredient1, ingredient2, ingredient3) {
+  orderPasta(ingredient1, ingredient2, ingredient3) {
     console.log(
       `Here is your delicious pasta with ${ingredient1}, ${ingredient2}, ${ingredient3}`
     );
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log('main ingredient', mainIngredient);
     console.log('others ingredients', otherIngredients);
   },
 };
 
+// ENHANCED OBJECT LITERALS
+
+/*
 // LOOPING ARRAYS
 
 const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
@@ -73,6 +84,7 @@ for (const menuItem of menu.entries()) {
 for (const [item, value] of menu.entries()) {
   console.log(`${item + 1}: ${value}`);
 }
+*/
 
 /*
 // LOGICAL ASSINGMENT OPERATORS ES2021
