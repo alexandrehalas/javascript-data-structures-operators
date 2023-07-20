b207ea1c3211f321e0f6bf94d18d3b1fc3719ea2
Alexandre Halas <alexandrehalas@gmail.com>
2023-07-20 18:11:50 -0300

    Code Challenge 4

--- codeChallenge.js
+++ codeChallenge.js
@@ -213,3 +213,65 @@ for (const [min, event] of [...gameEvents]) {
   const half = min <= 45 ? 'FIRST' : 'SECOND';
   console.log(`${half} HALF ${min}: ${event}`);
 }
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+a
+a_b_c
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const btn = document.querySelector('button');
+btn.innerHTML = 'Convert underscore_case to camelCase';
+btn.style.width = '200px';
+btn.style.height = '105px';
+
+btn.addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const values = row.toLowerCase().trim().split('_');
+
+    if (values.length !== 2) {
+      return row;
+    }
+
+    const [first, second] = values;
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
